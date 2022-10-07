454b7b715c1e79ef7197aff8ae0f04aff010dfa0
Qayrat Sultan <produtgsm@gmail.com>
2022-10-07 05:08:57 +0000

    message

--- main.py
+++ main.py
@@ -35,16 +35,16 @@ async def deleting_messages(msg: types.Message):
             await msg.answer("""Ushbu guruhga xush kelibsiz, <a href="tg://user?id={}">{}</a>\n
 Iltimos, WikiStipendiya marafoni haqida <a href="https://youtube.com/playlist?list=PLTiLidqsHkJMY1CSQiUuweKqly5LmYp_L">ushbu havola</a> orqali 
 tanishib chiqing.
-            
+ 
 <a href="https://uz.wikipedia.org/wiki/Vikipediya:WikiStipendiya_marafoni/WikiAyollar"> WikiAyollar sahifasi</a>
-            
+ 
 Loyiha doirasida yaratgan maqolalaringizni <a href="https://fountain.toolforge.org/editathons/wikiayollar">ushbu havola</a> orqali kiriting 
             """.
                              format(msg.new_chat_members[0].id, msg.from_user.full_name),
                              parse_mode="HTML")
         else:
             await msg.answer("""Ushbu guruhga xush kelibsiz, <a href="tg://user?id={}">{}</a>\n
-Iltimos, WikiStipendiya marafoni haqida <a href="https://youtube.com/playlist?list=PLTiLidqsHkJMY1CSQiUuweKqly5LmYp_L">ushbu havola</a> orqali 
+Iltimos, WikiStipendiya marafoni HAMDA VIKIPEDIYAGA HISSA QOʻSHISH (tahrirlash, maqola yaratish, fayl yuklash va hokazo) haqida <a href="https://youtube.com/playlist?list=PLTiLidqsHkJMY1CSQiUuweKqly5LmYp_L">ushbu havola</a> orqali 
 tanishib chiqing""".
                              format(msg.new_chat_members[0].id, msg.from_user.full_name),
                              parse_mode="HTML")
